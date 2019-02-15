5e243f41e0a2248d080dc3b3fbb3255886357e01
VJ <vraj416@gmail.com>
2019-02-15 18:47:22 -0500

    added url patterns in roster to link to a certain player within a specific team

--- SmartStats/roster/urls.py
+++ SmartStats/roster/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        #ex: /roster/team/5/player/4
+        path('team/<int:team_id>/player/<int:player_id>', views.player, name='player'),
         ]
--- SmartStats/roster/views.py
+++ SmartStats/roster/views.py
@@ -10,3 +10,7 @@ def coach(request):
 
 def team(request):
     return HttpResponse("Roster page - list of players and information")
+
+def player(request, team_id, player_id):
+    response="You're looking at player {0} who plays for team {1}."
+    return HttpResponse(response.format(player_id, team_id))
