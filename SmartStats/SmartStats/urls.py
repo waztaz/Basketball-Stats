aec0cd65885abc0fe4b2a2fc05752c58adf01979
VJ <vraj416@gmail.com>
2019-02-15 21:38:04 -0500

    Created second app called basketballstats which will encapsulate the statistics and added it to the urlpatterns

--- SmartStats/SmartStats/urls.py
+++ SmartStats/SmartStats/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('roster/', include('roster.urls'), name='roster'),
+    path('basketballstats/', include('basketball_stats.urls'), name='basketball_stats'),
 ]
