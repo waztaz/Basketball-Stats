bd837be780daa35b43ed0bb90319425dd00a459e
LucasAlva <lucasganoza@gmail.com>
2019-04-05 23:24:31 -0400

    Logic almost complete for registration and login, need to debug further

--- SmartStats/roster/urls.py
+++ SmartStats/roster/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
         path('', views.index, name='index'),
         path('home', views.home, name='home'),
         path('register', views.register, name='register'),
+        path('login', views.login, name='login'),
         #ex: /roster/team/5/player/4
         #path('team/<int:team_id>/player/<int:player_id>', views.player, name='player'),
         path('', views.coach, name='coach'),
