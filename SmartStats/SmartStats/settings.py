a3ee8ef5641a9b24814a5bbc90aaf6941643d592
Raghav <raghavw29@gmail.com>
2019-04-13 17:52:36 -0400

    fixing merge

--- SmartStats/SmartStats/settings.py
+++ SmartStats/SmartStats/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-<<<<<<< HEAD
-    'autofixture',
+
+    #'autofixture',
     'roster',
-=======
-    'roster.apps.RosterConfig',
+
+    #'roster.apps.RosterConfig',
     #'users.apps.UsersConfig',
->>>>>>> url_models_veijay
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
