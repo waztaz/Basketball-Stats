74d5e6173582aa47dacf8697e80b646817c0c9c0
Raghav <raghavw29@gmail.com>
2019-04-17 22:20:04 -0400

    added stuff

---
+++ SmartStats/home/static/home/js/coach.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+function add() 
+{
+   var x = document.getElementsByClassName("btn btn-primary btn-lg")
+   console.log(x)
+
+}
+
+$(".btn btn-primary btn-lg").click(function(){
+  alert("Text");
+});
+
+})
