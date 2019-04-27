3fc9bdd6a36cc337045ef32c8564caa3bc470b6f
LucasAlva <lucasganoza@gmail.com>
2019-04-26 20:27:50 -0400

    Temporarily removed playbook features to fix other issues

--- SmartStats/roster/forms.py
+++ SmartStats/roster/forms.py
@@ -6,12 +6,17 @@ from django.forms.utils import ValidationError
 from roster.models import (Player, Coach, User, Team)
 
 class PlayerSignUpForm(UserCreationForm):
+    POINT_GUARD = 'PG'
+    SHOOTING_GUARD = 'SG'
+    SMALL_FORWARD = 'SF'
+    POWER_FORWARD = 'PF'
+    CENTER = 'C'
     POSITION_CHOICES = (
-            ('Point Guard', '1'),
-            ('Shooting Guard', '2'),
-            ('Small Forward', '3'),
-            ('Power Forward', '4'),
-            ('Center', '5'),
+        (POINT_GUARD, "Point Guard"),
+        (SHOOTING_GUARD, "Shooting Guard"),
+        (SMALL_FORWARD, "Small Forward"),
+        (POWER_FORWARD, "Power Forward"),
+        (CENTER, "Center"),
     )
 
 
@@ -19,7 +24,7 @@ class PlayerSignUpForm(UserCreationForm):
     last_name=forms.CharField(label='Last Name', max_length=50)
     height=forms.IntegerField(label='Height in inches')
     weight=forms.IntegerField(label='Weight in lbs')
-    #position=forms.ChoiceField(choices=POSITION_CHOICES)
+    position=forms.ChoiceField(choices=POSITION_CHOICES)
 
     class Meta(UserCreationForm.Meta):
         model = User
