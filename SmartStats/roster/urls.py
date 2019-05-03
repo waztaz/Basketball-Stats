58e95ccdd1822d64aa66f294be5480ce6d09d4f9
VJ <vraj416@gmail.com>
2019-05-03 09:00:10 -0400

    Added schedule button

--- SmartStats/roster/urls.py
+++ SmartStats/roster/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
             path('team/<int:pk>/schedule/', coaches.GameListView.as_view(), name='schedule'),
             path('team/<int:pk>/player/add', coaches.player_add, name='player_add'),
             path('team/<int:team_pk>/player/<int:player_pk>/', coaches.player_change, name='player_change'),
-            path('team/<int:team_pk>/real_time_tracker', coaches.real_time_tracker, name='real_time_tracker')
+            path('team/<int:team_pk>/game/<int:game_pk>/real_time_tracker', coaches.real_time_tracker, name='real_time_tracker')
             ], 'roster'), namespace='coaches')),
 
         path('players/', include(([
