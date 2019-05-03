fa03683cf7d960f1daebf44848510b65063688ce
LucasAlva <lucasganoza@gmail.com>
2019-05-03 06:58:07 -0400

    Test Cases

--- SmartStats/roster/tests.py
+++ SmartStats/roster/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from myapp.models import Coach, Team, Player, Scout, Game, CumulativeStats, BasketballStats
 
 # Create your tests here.
+class CoachTestCase(TestCase):
+	def setUp(self):
+		Coach.objects.create(first_name="Test" , last_name="Coach")
+
+	def test_coach_exists(self):
+		"Testing for properly generated coach"
+		#coach = Coach.objects.get()
+
+class TeamTestCase(TestCase):
+	def setUp(self):
+		Team.objects.create(name="Cavaliers")
+
+	#def test_coach_exists(self):
+
+
+class PlayerTestCase(TestCase):
+	def setUp(self):
+		Player.objects.create(team="Cavaliers", first_name="Test", last_name="Player", height=68, weight=190, position=SG)
+
+class ScoutTestCase(TestCase):
+	def setUp(self):
+		Scout.objects.create(first_name="Test" , last_name="Scout")
+
+#class GameTestCase(TestCase):
+#	def setUp(self):
+#		Game.objects.create(first_name="Test" , last_name="Coach")
+
+#class CumulativeStatsTestCase(TestCase):
+#	def setUp(self):
+
+#class BasketballStatTestCase(TestCase):
+#	def setUp(self):
