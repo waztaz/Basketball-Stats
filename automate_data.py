1c5e4df5fc138f746b21137cb773ed4c82500def
Raza Agha <waztaz@gmail.com>
2019-08-31 15:41:50 -0400

    Removing empyt directories

--- automate_data.py
+++
@@ -1,3 +0,0 @@
-#automate data collection here - output to JSON
-#run manage.py loaddate <fixturename> afterwards on JSON file
-
--- basketball_stats/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BasketballStatsConfig(AppConfig):
-    name = 'basketball_stats'
